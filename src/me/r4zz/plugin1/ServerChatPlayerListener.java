90457bc028b299100c0a20a4a9a509e1f225f0bc
Ryan Ellis <R4ZZ96@hotmail.co.uk>
2011-10-29 22:40:51 +0100

    first commit possibly not working

---
+++ src/me/r4zz/plugin1/ServerChatPlayerListener.java
@@ -0,0 +1,26 @@
+package me.r4zz.plugin1;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerListener;
+
+public class ServerChatPlayerListener extends PlayerListener {
+	public static plugin1 plugin;
+	
+	public ServerChatPlayerListener(plugin1 instance) {
+		plugin = instance;
+	}
+	
+	public void onPLayerChat(PlayerChatEvent chat) {
+		Player p = chat.getPlayer();
+		String message = chat.getMessage();
+		String message_lower = message.toLowerCase();
+		ChatColor RED = ChatColor.RED;
+		ChatColor WHITE = ChatColor.WHITE;
+		if(message_lower.contains("hi") && message_lower.contains("server")) {
+			p.sendMessage(RED + "[Server]" + WHITE + "Hello " + p.getName());
+			chat.setCancelled(true);
+		}
+	}
+}
---
+++ src/me/r4zz/plugin1/plugin1.java
@@ -0,0 +1,33 @@
+package me.r4zz.plugin1;
+
+import java.util.logging.Logger;
+
+import me.r4zz.plugin1.plugin1;
+import me.r4zz.plugin1.ServerChatPlayerListener;
+
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class plugin1 extends JavaPlugin {
+	
+	public static plugin1 plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+	public final ServerChatPlayerListener playerListener = new ServerChatPlayerListener(this);
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " is now disabled.");
+	}
+	
+	@Override
+	public void onEnable() {
+		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_CHAT, this.playerListener, Event.Priority.Normal, this);
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled.");
+	}
+
+}
